8f7afec384e669a0ae4247f9645a50b43a88ebf2
pireddag <48107401+pireddag@users.noreply.github.com>
2020-03-24 15:27:55 +0100

    Blocked the too-dangerous load
    Working on filtering commands

--- progs/insertGraphics/insertGraphics.scm
+++ progs/insertGraphics/insertGraphics.scm
@@ -2,5 +2,8 @@
 
 (tm-define (insertGraphics graphicsFile)
 	   (set! graphicsFile (tree->stree graphicsFile))
-	   (load graphicsFile)
-	   (stree->tree SchemeGraphics))
+	   ;; The following two lines are commented out at the moment as they are too dangerous (allow the running of any code)
+	   ;; Working on how to filter the commands allowed through load
+;;	   (load graphicsFile)
+;;	   (stree->tree SchemeGraphics)
+	   )
